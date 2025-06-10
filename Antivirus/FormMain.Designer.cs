a5d9552523b3027bde12f665d7176964938cefad
geor <georguzitaev@gmail.com>
2025-06-10 11:13:20 +0300

    Интерфейс первой формы

--- Antivirus/FormMain.Designer.cs
+++ Antivirus/FormMain.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxListOfFiles = new System.Windows.Forms.TextBox();
+            this.buttonAddNewFile = new System.Windows.Forms.Button();
+            this.buttonCalculate = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxListOfFiles
+            // 
+            this.textBoxListOfFiles.Location = new System.Drawing.Point(22, 33);
+            this.textBoxListOfFiles.Multiline = true;
+            this.textBoxListOfFiles.Name = "textBoxListOfFiles";
+            this.textBoxListOfFiles.Size = new System.Drawing.Size(269, 394);
+            this.textBoxListOfFiles.TabIndex = 0;
+            // 
+            // buttonAddNewFile
+            // 
+            this.buttonAddNewFile.Location = new System.Drawing.Point(364, 103);
+            this.buttonAddNewFile.Name = "buttonAddNewFile";
+            this.buttonAddNewFile.Size = new System.Drawing.Size(198, 93);
+            this.buttonAddNewFile.TabIndex = 1;
+            this.buttonAddNewFile.Text = "Добавить";
+            this.buttonAddNewFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(364, 261);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(198, 93);
+            this.buttonCalculate.TabIndex = 2;
+            this.buttonCalculate.Text = "Рассчитать";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.buttonAddNewFile);
+            this.Controls.Add(this.textBoxListOfFiles);
             this.Name = "FormMain";
             this.Text = "Антивирус";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxListOfFiles;
+        private System.Windows.Forms.Button buttonAddNewFile;
+        private System.Windows.Forms.Button buttonCalculate;
     }
 }
 
